8406f05497f2ddc51f8b6fde54bc21a80eb3fb8b
Liannium <meliaerinelizabeth@gmail.com>
2020-11-18 00:42:34 -0800

    finished week display

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WeatherMood | Welcome' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'WeatherMood | Home'});
+});
+
 module.exports = router;
