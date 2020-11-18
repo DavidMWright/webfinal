44ce2f4baca16666d8b7b4e1f3788b3eda2a5e2a
huyn7539 <47341426+huyn7539@users.noreply.github.com>
2020-11-18 15:07:02 -0800

    Got the url to correctly load and the corrected api key need to get info from server

--- weather.js
+++ weather.js
@@ -1,17 +1,23 @@
 
-let GetLocation = 'city';
-let unit = 'imperial';
-let weatherID = '9dc9eeb95e5bf155ec332b2150dc17ae';
+var searchLocation = 'zip';
+var unit = 'imperial';
+var weatherID = '108e39011b5fb4c189360456ae96f6d5';
 
 document.getElementById('searchBtn').addEventListener('click', () => {
-    let getCity = document.getElementById('searchData').nodeValue;
+    let getCity = document.getElementById('searchData').value;
     if(getCity) {
         getWeather(getCity);
     }
 })
 
 function getWeather(getCity) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${getLocation}={GetCity}&ID=${weatherID}&unit=${unit}`).then(result => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchLocation}=${getCity}&APPID=${weatherID}&units=${unit}`).then(result => {
         return result.json();
-    }).then(result => {console.log(result)});
+    }).then(result => {
+        displayInfo(result);
+    })
+}
+
+function displayInfo(serverResult) {
+    document.getElementById("currentTemp").innerHTML = Math.round(serverResult.main.temp);
 }
