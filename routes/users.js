ddfce7cf4c5d4c51ea4b271eb3b658e0ea317e36
DavidMWright <47340977+DavidMWright@users.noreply.github.com>
2020-11-21 21:02:34 -0800

    set up some testing things

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 var user_controller = require('../controllers/userController');
 
-/* Post Request for User Page */
-router.post('/users/:id', user_controller.user_detail);
+// Post Request for User Page
+router.post('/:id', user_controller.user_detail);
 
+// Only set up for testing 
+router.get('/', function(req, res, next) {
+    res.render('profile', { user: {first_name: 'David', last_name: 'Wright'} });
+});
 
 module.exports = router;
