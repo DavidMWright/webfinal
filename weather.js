398cc4ce8f91989d1def8dbff636a229b2210338
huyn7539 <47341426+huyn7539@users.noreply.github.com>
2020-11-22 15:09:49 -0800

    got icons working as well as ° to show

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 
-var searchLocation = 'zip';
+var searchLocation = 'q';
 var unit = 'imperial';
 var weatherID = '108e39011b5fb4c189360456ae96f6d5';
 
@@ -19,5 +19,31 @@ function getWeather(getCity) {
 }
 
 function displayInfo(serverResult) {
-    document.getElementById("currentTemp").innerHTML = Math.round(serverResult.main.temp);
+    
+    switch(serverResult.weather[0].main) {
+        case 'Clear':
+        break;
+        case 'Rain':
+        break;
+        case 'Clouds':
+        break;
+        case 'Drizzle':
+        break;
+        case 'Mist':
+        break;
+        case 'Thundertorm':
+        break;
+        case 'Snow':
+        break;
+    }
+
+    let weatherDescriptionElement = document.getElementById('weatherDescriptionElement');
+    let temperatureElement = document.getElementById('currentTemp');
+    let cityElement = document.getElementById('cityHeader');
+    let weatherIconElement = document.getElementById('documentIconElement');
+    let weatherDescriptionResult = serverResult.weather[0].description;
+
+    weatherIconElement.src = 'http://openweathermap.org/img/w/' + serverResult.weather[0].icon + '.png';
+    weatherDescriptionElement.innerText = weatherDescriptionResult.charAt(0).toUpperCase() + weatherDescriptionResult.slice(1);
+    temperatureElement.innerText = Math.floor(serverResult.main.temp) + String.fromCharCode(176);
 }
