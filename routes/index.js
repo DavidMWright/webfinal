18e7c494533103e135a0a3ff51e3985bad9e3cb9
DavidMWright <47340977+DavidMWright@users.noreply.github.com>
2020-11-23 20:17:20 -0800

    Session stuff mostly setup

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,16 @@ var index = require('../controllers/indexController');
 var router = express.Router();
 
 // GET Sign in page.
-router.get('/', index.sign_in);
+router.get('/', index.sign_in_page);
+
+// POST Sign in page
+router.post('/', index.sign_up);
 
 // POST home page
-router.post('/home', index.home_page);
+router.post('/signin', index.sign_in);
 
-router.get('/signup', index.sign_up);
+router.get('/signup', index.sign_up_page);
+
+router.get('/home', index.home);
 
 module.exports = router;
