b01e527f42a3ac136d0c2e55e138dbc85417e35e
DavidMWright <47340977+DavidMWright@users.noreply.github.com>
2020-11-23 20:46:47 -0800

    merge conflicts resolved

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,16 @@ var router = express.Router();
 // GET Sign in page.
 router.get('/', index.sign_in_page);
 
-// POST Sign in page
-router.post('/', index.sign_up);
-
-// POST home page
+// POST to home page
 router.post('/signin', index.sign_in);
 
+// GET Sign in page
 router.get('/signup', index.sign_up_page);
 
+// POST Sign in page after sign up
+router.post('/', index.sign_up);
+
+// GET Home page
 router.get('/home', index.home);
 
 module.exports = router;
